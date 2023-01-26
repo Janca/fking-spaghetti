724729aec58a885edf8d67198b686c7898ea0a4c
fking <janca@fking.gay>
2023-01-26 12:21:37 -0600

    In a weird state of mismatched ideas, main program not broken, but don't look at ui2.py

--- fking/widgets.py
+++ fking/widgets.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.ttk
 from typing import Union
 
 
@@ -26,3 +27,50 @@ def x_spacer(master, size: int) -> tkinter.Widget:
 
 def y_spacer(master, size: int) -> tkinter.Widget:
     return spacer(master, size, tkinter.VERTICAL)
+
+
+def label_separator(master, label: Union[str, tkinter.StringVar]) -> tkinter.Widget:
+    wrapper = tkinter.Frame(master)
+    is_string_var = isinstance(label, tkinter.StringVar)
+
+    tkinter.ttk.Label(
+            wrapper,
+            text=label if not is_string_var else None,
+            textvariable=label if is_string_var else None
+    ).pack(side=tkinter.LEFT, anchor=tkinter.W, fill=tkinter.Y, expand=False)
+    x_spacer(wrapper, 3).pack(side=tkinter.LEFT, expand=False)
+    tkinter.ttk.Separator(
+            wrapper,
+            orient=tkinter.HORIZONTAL
+    ).pack(side=tkinter.LEFT, anchor=tkinter.W, fill=tkinter.X, expand=True)
+
+    return wrapper
+
+
+def toggle_button(master, **kwargs) -> tkinter.ttk.Button:
+    button = tkinter.ttk.Button(master, **kwargs)
+    button.toggled = False
+
+    def do_toggle(self: tkinter.ttk.Button):
+        self.toggled = not self.toggled
+        toggle(self, self.toggled)
+
+        return "break"
+
+    def toggle(self: tkinter.ttk.Button, selected: bool):
+        self.toggled = selected
+        self.state(["pressed" if selected else "!pressed"])
+
+    button.bind("<ButtonRelease-1>", lambda e: do_toggle(button))
+    button.toggle = toggle.__get__(button)
+
+    return button
+
+
+def layout_form_row(master: tkinter.Widget, *args: tkinter.Widget) -> tkinter.Widget:
+    for i, widget in enumerate(args, start=1):
+        if i == len(args):
+            widget.pack(side=tkinter.RIGHT, anchor=tkinter.E, fill=tkinter.Y, expand=False)
+        else:
+            widget.pack(side=tkinter.LEFT, anchor=tkinter.W, fill=tkinter.BOTH, expand=True)
+    return master
